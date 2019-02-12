79e59301337039ae9abcd0bad85140158427457a
Huang-Ming Huang <huangh@objectcomputing.com>
2019-02-11 22:07:07 -0600

    add clang libstdc++11 build

--- build.py
+++ build.py
@@ -2,10 +2,19 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+from cpt.packager import ConanMultiPackager
+import platform
 
 if __name__ == "__main__":
-
-    builder = build_template_default.get_builder(pure_c=False)
-
+    builder = ConanMultiPackager()
+    builder.add_common_builds(pure_c=False)
+    if platform.system() == "Linux":
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            if settings["compiler"] == "clang" and settings["compiler.libcxx"] == "libstdc++":
+                settings_libstdcxx11 = settings.copy()
+                settings_libstdcxx11["compiler.libcxx"] = "libstdc++11"
+                filtered_builds.append([settings_libstdcxx11, options, env_vars, build_requires])
+            filtered_builds.append([settings, options, env_vars, build_requires])
+        builder.builds = filtered_builds
     builder.run()
